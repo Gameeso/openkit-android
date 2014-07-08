63a06cc236940354911348849b66d0ca9d3145f3
Peter Wilemsen <peter@codebuffet.co>
2014-07-08 16:02:40 +0200

    Squashed commits

--- travis/script.sh
+++ travis/script.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-mkdir output
-mv LICENSE.txt output
-mv OpenKitSDK output
-mv README.md output
-mv samples output
-mv Vendor output
-
-zip -r Gameeso-Android-SDK.zip output/
+echo "Creating SDK zip file"
+zip -r Gameeso-Android-SDK.zip LICENSE.txt OpenKitSDK README.md samples Vendor
