c49c6f3a1ed7df4a923900e78a02a63917e7575d
Peter Wilemsen <peter@codebuffet.co>
2014-07-09 16:59:39 +0200

    Testing new build system

--- travis/script.sh
+++ travis/script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+echo "Building Android SDK"
+ant $ANT_CONFIG -f $ANDROID_SDK_BUILD_FILE
+
+echo "Building Unity Android SDK"
+ant $ANT_TARGET -f $ANDROID_UNITY_BUILD_FILE
+
 echo "Creating SDK zip file"
 zip -r Gameeso-Android-SDK.zip LICENSE.txt OpenKitSDK README.md samples Vendor
